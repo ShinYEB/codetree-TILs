21efe9b68b69095035a99ade7c646b15b358d4a2
ShinYEB <91859242+ShinYEB@users.noreply.github.com>
2024-07-11 22:07:23 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240711/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool ismove(int n, int y, int x)
+{
+    return x>=0 && y>=0 && x<n && y<n;
+}
+
+pair<int, int> move(vector<vector<int>> A, int n, int y, int x)
+{
+    int ans = 0;
+    
+    if (ismove(n, y-1, x) && A[y-1][x] > ans)
+    {
+        ans = A[y-1][x];
+        y = y-1;
+    }
+    else if (ismove(n, y+1, x) && A[y+1][x] > ans)
+    {
+        ans = A[y+1][x];
+        y = y+1;
+    }
+    else if (ismove(n, y, x-1) && A[y][x-1] > ans)
+    {
+        ans = A[y][x-1];
+        x = x-1;
+    }
+    else if (ismove(n, y, x+1) && A[y][x+1] > ans)
+    {
+        ans = A[y][x+1];
+        x = x+1;
+    }       
+    return make_pair(y, x);
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, m, t, tempi;
+    int r, c;
+    int ans;
+    bool is_continue = true;
+    vector<vector<int>> A, board;
+    vector<pair<int, int>> start, tempv;
+    vector<int> temp, tempb;
+
+    cin >> n >> m >> t;
+    
+    for (int i=0; i<n; i++)
+    {
+        temp.clear();
+        for (int j=0; j<n; j++)
+        {
+            cin >> tempi;
+            temp.push_back(tempi);
+            tempb.push_back(0);
+        }
+        A.push_back(temp);
+        board.push_back(tempb);
+    }
+    
+    for (int i=0; i<m; i++)
+    {
+        cin >> r >> c;
+        start.push_back(make_pair(r, c));
+    }
+
+
+    for (int i=0; i<t; i++)
+    {
+        tempv.clear();
+        
+        for (int j=0; j<start.size(); j++)
+        {
+            pair<int, int> tempp = move(A, n, start[j].first, start[j].second);
+            r = tempp.first;
+            c = tempp.second;
+            
+            board[r][c] = board[r][c] + 1;
+        }
+
+        for (int y=0; y<n; y++)
+            for (int x=0; x<n; x++)
+                {
+                    if (board[y][x] == 1)
+                        tempv.push_back(make_pair(y, x));
+                    board[y][x] = 0;
+                }
+        start.clear();
+        start = tempv;
+    }
+
+    cout << start.size();
+    return 0;
+
+}
