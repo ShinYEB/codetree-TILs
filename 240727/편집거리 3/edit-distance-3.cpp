b8c551a4351a9c53200123803d5f3a08a512af18
ShinYEB <91859242+ShinYEB@users.noreply.github.com>
2024-07-27 00:54:28 +0900

    Title: 편집거리 3, Time: 10ms, Memory: 3MB, Status: Passed - CodeTree

--- 240727/편집거리 3/edit-distance-3.cpp
+++ 240727/편집거리 3/edit-distance-3.cpp
@@ -6,20 +6,26 @@ int main() {
     string str1, str2;
     cin >> str1 >> str2;
     
-    int str1_size = str1.size();
-    int str2_size = str2.size();
+    int str1_size = str1.length();
+    int str2_size = str2.length();
 
     int **dp = new int*[str1_size+1];
     for(int i=0; i<=str1_size; i++)
         dp[i] = new int[str2_size+1];
     
+    for(int i=0; i<=str1_size; i++)
+        dp[i][0] = i;
+
+    for(int i=0; i<=str2_size; i++)
+        dp[0][i] = i;
+
     for(int i=0; i<str1_size; i++)
         for(int j=0; j<str2_size; j++)
         {
             if(str1[i] == str2[j])
-                dp[i+1][j+1] = dp[i][j] + 1;
+                dp[i+1][j+1] = dp[i][j];
             else
-                dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j]);
+                dp[i+1][j+1] = min(dp[i][j+1], dp[i+1][j]) + 1;
         }
 
     cout << dp[str1_size][str2_size];
